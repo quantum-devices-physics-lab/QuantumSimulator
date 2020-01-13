347e6d760afa53de6ce8f38ed0976b8d254a999a
Lucas Medeiros <lucamedeir@gmail.com>
2020-01-13 16:55:50 -0300

    Added commentaries

--- QuantumSimulator.py
+++ QuantumSimulator.py
@@ -1,3 +1,41 @@
+#
+# Quantum Simulation Script
+# This script simulates two cavities, both coupled to a resonator (all three harmonic oscillators).
+# The cavities are labeled cavity a and cavity b while the resonator is labeled resonator r.
+#
+# The function that actually compute the simulation is called execute.
+# It receives the simulation parameters, for example, cavities frequency, fock number, etc, and
+# returns an object instantiated from the class SimulationData, which holds the parameters given
+# and the density operator, purity, and the destruction operators for a, b and r.
+#
+# The function execute can be called directly, however to use parallelism, one must call
+# the simulate function, which in turn calls the execute internally. The function simulate receives as paremeter
+# a string that labels all simulation data, for example, "Simulation 1", and also a list of tasks, which are
+# dictionary holding the parameters for the execute function.
+#
+# Each task in the task list will be computed by the execute function in parallel.
+#
+# This an example of a task
+#
+#   {"N":N, ----------------> fock number
+#    "ωa":ωa, --------------> cavity a frequency
+#    "ωb":ωb, --------------> cavity b frequency
+#    "ωr":ωr, --------------> resonator r frequency
+#    "ga":ga, --------------> cavity a coupling coefficient
+#    "gb":gb, --------------> cavity b coupling coefficient
+#    "κa":κa, --------------> cavity a dissipation rate
+#    "κb":κb, --------------> cavity b dissipation rate
+#    "κr":κr, --------------> resonator r dissipation rate
+#    "T":T, ----------------> System temperature
+#    "A":A, ----------------> Drive frequency Amplitude
+#    "ωd_begin":ωd_begin, --> drive sweep initial frequency
+#    "ωd_end":ωd_end, ------> drive sweep final frequency
+#    "n_points":n_points, --> number of sweep frequency points
+#    "idx":idx, ------------> frequency points number
+#    "name":name} ----------> label for this specific task
+#
+# 
+
 from qutip import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,14 +49,16 @@ import os
 import re
 import sys
 
-
+# Returns the average photon number for a given temperature and frequency.
 def calculate_n_th(T,ω):
     return 1/(np.exp(sc.hbar*ω/(sc.k*T))-1)
 
+# Return the system hamiltonian.
 def drive_Hamiltonian(a,ωa,b,ωb,r,ωr,ga,gb,ωd,A):
-    H= (ωa-ωd)*a.dag()*a + ωb*b.dag()*b + ωr*r.dag()*r + A*(a.dag()+a) - ga*a.dag()*a*(r.dag()+r) - gb*b.dag()*b*(r.dag()+r)
+    H = (ωa-ωd)*a.dag()*a + ωb*b.dag()*b + ωr*r.dag()*r + A*(a.dag()+a) - ga*a.dag()*a*(r.dag()+r) - gb*b.dag()*b*(r.dag()+r)
     return H
 
+# Add the collapse operator for the Master equation.
 def add_c_ops(c_ops,rate_excitation,rate_relaxation,op):
     if rate_excitation > 0.0:
         c_ops.append(np.sqrt(rate_excitation)*op.dag())
@@ -28,6 +68,8 @@ def add_c_ops(c_ops,rate_excitation,rate_relaxation,op):
         
     return c_ops
 
+# A class that is actually a struct of the data produced by the simulation.
+# The really important variable is rho. The rest is for management simplicity.
 class SimulationData():
     def __init__(self,task,a,expect_a,purity,b,r,rho):
         self.task = task
@@ -38,7 +80,7 @@ class SimulationData():
         self.b = b
         self.r = r
 
-        
+# Call this function to create a new task.
 def create_task(N,ωa,ωb,ωr,ga,gb,κa,κb,κr,T,A,ωd_begin,ωd_end,n_points,idx,name):
     return {"N":N,
             "ωa":ωa,
@@ -57,8 +99,11 @@ def create_task(N,ωa,ωb,ωr,ga,gb,κa,κb,κr,T,A,ωd_begin,ωd_end,n_points,i
             "idx":idx,
             "name":name}
 
+
+# Main processing function execute.
 def execute(N,ωa,ωb,ωr,ga,gb,κa,κb,κr,T,A,ωd_begin,ωd_end,n_points,name,dirName):
-    
+
+    # Calculate the average number of photons. Used to calculate the excitation and relaxation rate below.
     n_th_a = calculate_n_th(T,ωa)
     n_th_b = calculate_n_th(T,ωb)
     n_th_r = calculate_n_th(T,ωr)
@@ -70,11 +115,13 @@ def execute(N,ωa,ωb,ωr,ga,gb,κa,κb,κr,T,A,ωd_begin,ωd_end,n_points,name,
     rate_excitation_a = κa*(n_th_a)
     rate_excitation_b = κb*(n_th_b)
     rate_excitation_r = κr*(n_th_r)
-    
+
+    # The destruction operator
     a = tensor(destroy(N),qeye(N),qeye(N))
     b = tensor(qeye(N),destroy(N),qeye(N))
     r = tensor(qeye(N),qeye(N),destroy(N))
 
+    # Creating the list of collapse operators
     c_ops = []
 
     if rate_excitation_a > 0.0:
@@ -95,15 +142,20 @@ def execute(N,ωa,ωb,ωr,ga,gb,κa,κb,κr,T,A,ωd_begin,ωd_end,n_points,name,
     if rate_relaxation_r > 0.0:
         c_ops.append(np.sqrt(rate_relaxation_r)*r)
         
-   
-    
+    # For each n_points between ωd_begin and ωd_end, the steady state is calculated.
+    # The purity and expectation value of a are also calculated.
     ωds = np.linspace(ωd_begin,ωd_end,n_points)
     for idx,ωd in enumerate(ωds):
+
         H = drive_Hamiltonian(a,ωa,b,ωb,r,ωr,ga,gb,ωd,A)
+
+        # The steadysate function from QuTiP
         rho_ss = steadystate(H, c_ops)
+        
         purity = (rho_ss*rho_ss).tr()
         expect_a = (rho_ss*a).tr()
-        
+
+        # Create an instance of SimulationData class
         data = SimulationData(create_task(N,ωa,ωb,ωr,ga,gb,κa,κb,κr,T,A,ωd_begin,ωd_end,n_points,idx,name),
                               a,
                               expect_a,
@@ -111,7 +163,9 @@ def execute(N,ωa,ωb,ωr,ga,gb,κa,κb,κr,T,A,ωd_begin,ωd_end,n_points,name,
                               b,
                               r,
                               rho_ss)
-        
+
+        # Save the data. The name of the file will be a combination of the name of the simulation, plus and the
+        # name of the task and the number of the point calculated.
         filedata_name = "{}/data_{}_{}".format(dirName,data.task["name"],data.task["idx"])
         file = open(filedata_name,"wb")
         print("Saving acquired data {} point {}".format(data.task["name"],data.task["idx"]))
@@ -120,29 +174,30 @@ def execute(N,ωa,ωb,ωr,ga,gb,κa,κb,κr,T,A,ωd_begin,ωd_end,n_points,name,
         
     return data
 
+# The parallel function simulate.
 def simulate(name,tasks):
-    
+
+    # This is a setup for the logging system
     filename = name.replace(" ","_")
-    
     logging.basicConfig(level=logging.DEBUG) 
     logger = logging.getLogger(name)
-
     handler = logging.FileHandler('log_{}_{}.log'.format(filename,datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")))
     handler.setLevel(logging.INFO)
-
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-
     logger.addHandler(handler)
-    
+
+    # Obtain the number of cpus to be used
     task_count = len(tasks)
-    
     cpu_count = mp.cpu_count()
-        
-    logger.info("Starting Simulation")
+
     
+    logger.info("Starting Simulation")
+
+    # Define the name of the folder where the files are going to be stored
     dirName = "data_{}_{}".format(filename,datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
-    # Create target Directory if don't exist
+    
+    # Create target Directory if one does not exist
     if not os.path.exists(dirName):
         os.mkdir(dirName)
         logger.info("Directory {} Created ".format(dirName))
@@ -150,11 +205,13 @@ def simulate(name,tasks):
         logger.info("Directory {} already exists".format(dirName))
 
     logger.info("#CPU {}".format(cpu_count))
-    
+
+    # Parellel code
     try:
         t_start = time.time()
         time_1 = []
 
+        # get a pool object and apply the function execute for each task.
         pool = mp.Pool(processes = cpu_count)
 
         results = [pool.apply_async(execute,args=(task["N"],
@@ -174,6 +231,7 @@ def simulate(name,tasks):
                                                   task["name"],
                                                   dirName),callback=None,error_callback=None) for task in tasks]
 
+        # For each hour passed, the progress is logged. The loops continue until all tasks have been finished.
         passedAnHour = 0
         while True:
             incomplete_count = sum(1 for x in results if not x.ready())
@@ -194,6 +252,8 @@ def simulate(name,tasks):
             
             time.sleep(1)
 
+        
+        # When it is finished, get all the data.
         while not all([ar.ready() for ar in results]):
             for ar in results:
                 ar.wait(timeout=0.1)
@@ -201,16 +261,9 @@ def simulate(name,tasks):
 
         pool.close()
         pool.join
-
-        
         
         for ar in results:
             data = ar.get()
-        #    logger.info("Saving acquired data {} point {}".format(data.task["name"],data.task["idx"]))
-        #    filedata_name = "{}/data_{}_{}".format(dirName,data.task["name"],data.task["idx"])
-        #    file = open(filedata_name,"wb")
-        #    pickle.dump(ar.get(),file)
-        #    file.close()
 
     except Exception as e:
         logger.exception(e)
